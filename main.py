e47e9f75576a3d9809387de62e15d4a9f61bb5a7
MysticDespair <imogengracewright@hotmail.co.uk>
2026-06-17 20:54:42 +0100

    Player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_TURN_SPEED, PLAYER_SPEED
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -8,6 +9,10 @@ def main():
     pygame.display.set_caption("Asteroids")
     clock = pygame.time.Clock()
     dt = 0.0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
         log_state()
@@ -15,7 +20,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+
         screen.fill("black")
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
